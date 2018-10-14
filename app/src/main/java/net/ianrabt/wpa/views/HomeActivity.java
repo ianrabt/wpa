9e7437f021f9c20f4a74af79a6346530d95d0904
MichaelHamlett <mhamlett@hmc.edu>
2018-10-14 15:50:16 -0700

    Add Google Sign In and Sign Out

--- app/src/main/java/net/ianrabt/wpa/views/HomeActivity.java
+++ app/src/main/java/net/ianrabt/wpa/views/HomeActivity.java
@@ -1,15 +1,49 @@
 package net.ianrabt.wpa.views;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 import net.ianrabt.wpa.R;
 
 public class HomeActivity extends AppCompatActivity {
 
+    FirebaseAuth mAuth;
+    FirebaseAuth.AuthStateListener mAuthListener;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Button btn = (Button)findViewById(R.id.sign_out_button);
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if(firebaseAuth.getCurrentUser() == null){
+                    startActivity(new Intent(HomeActivity.this, LoginActivity.class));
+                }
+            }
+        };
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.signOut();
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
     }
 }
