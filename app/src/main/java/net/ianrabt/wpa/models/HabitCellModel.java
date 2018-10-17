b49518212f72992470f4e5d95cc719ab22f113e1
Jacqui Giese <jacqui.giese@gmail.com>
2018-10-17 15:36:15 -0700

    define HabitCellModel and write basic tests

--- app/src/main/java/net/ianrabt/wpa/models/HabitCellModel.java
+++ app/src/main/java/net/ianrabt/wpa/models/HabitCellModel.java
@@ -1,4 +1,31 @@
 package net.ianrabt.wpa.models;
 
-public class HabitCellModel {
+import android.arch.lifecycle.ViewModel;
+import java.util.Date;
+
+public class HabitCellModel extends ViewModel {
+    private String habitName;
+    private Date habitDate;
+    private int streakCounter;
+
+
+    public HabitCellModel(String habitName, Date habitDate) {
+        this.habitName = habitName;
+        this.habitDate = habitDate;
+        this.streakCounter = 0;
+    }
+
+    /* Getter Methods */
+    public String getHabitName() {
+        return this.habitName;
+    }
+
+    public Date getHabitDate(){
+        return this.habitDate;
+    }
+
+    public int getStreakCounter() {
+        return streakCounter;
+    }
+
 }
