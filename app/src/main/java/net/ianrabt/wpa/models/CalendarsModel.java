1a08432352dd214e9850a160ac2b3d268e42d23d
Ian Taylor <itaylor@hmc.edu>
2018-10-25 21:53:41 -0700

    add (broken) permissions work

--- app/src/main/java/net/ianrabt/wpa/models/CalendarsModel.java
+++ app/src/main/java/net/ianrabt/wpa/models/CalendarsModel.java
@@ -1,17 +1,21 @@
 package net.ianrabt.wpa.models;
 
+import android.Manifest;
 import android.arch.lifecycle.LiveData;
 import android.arch.lifecycle.MutableLiveData;
 import android.arch.lifecycle.ViewModel;
 import android.content.ContentResolver;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.net.Uri;
 import android.provider.CalendarContract.Calendars;
+import android.support.v4.app.ActivityCompat;
 
 public class CalendarsModel extends ViewModel {
     // Projection array. Creating indices for this array instead of doing
     // dynamic lookups improves performance.
-    public static final String[] EVENT_PROJECTION = new String[] {
+    public static final String[] EVENT_PROJECTION = new String[]{
             Calendars._ID,                           // 0
             Calendars.ACCOUNT_NAME,                  // 1
             Calendars.CALENDAR_DISPLAY_NAME,         // 2
@@ -29,5 +33,41 @@ public class CalendarsModel extends ViewModel {
     public CalendarsModel(Context context) {
         Cursor cur = null;
         ContentResolver cr = context.getContentResolver();
+
+        String[] selectionArgs = new String[]{};
+        Uri uri = Calendars.CONTENT_URI;
+        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.READ_CALENDAR) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            // No explanation needed; request the permission
+            ActivityCompat.requestPermissions(context,
+                    new String[]{Manifest.permission.READ_CONTACTS},
+                    MY_PERMISSIONS_REQUEST_READ_CONTACTS);
+
+            return TODO;
+        }
+        cur = cr.query(uri, EVENT_PROJECTION, "", selectionArgs, null);
+
+        // Use the cursor to step through the returned records
+        while (cur.moveToNext()) {
+            long calID = 0;
+            String displayName = null;
+            String accountName = null;
+            String ownerName = null;
+
+            // Get the field values
+            calID = cur.getLong(PROJECTION_ID_INDEX);
+            displayName = cur.getString(PROJECTION_DISPLAY_NAME_INDEX);
+            accountName = cur.getString(PROJECTION_ACCOUNT_NAME_INDEX);
+            ownerName = cur.getString(PROJECTION_OWNER_ACCOUNT_INDEX);
+
+            // Do something with the values...
+
+        }
     }
 }
--- app/src/main/java/net/ianrabt/wpa/views/HomeActivity.java
+++ app/src/main/java/net/ianrabt/wpa/views/HomeActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import net.ianrabt.wpa.R;
+import net.ianrabt.wpa.models.CalendarsModel;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -11,5 +12,7 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        CalendarsModel model = new CalendarsModel(getApplicationContext());
     }
 }
