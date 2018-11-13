0bbaea9f23b489a9cdbd7f1df189b1d7a762c08b
MichaelHamlett <mhamlett@hmc.edu>
2018-11-13 11:56:24 -0800

    Rework architecture to follow MVP pattern more closely

---
+++ app/src/main/java/net/ianrabt/wpa/controllers/HabitsController.java
@@ -0,0 +1,50 @@
+package net.ianrabt.wpa.controllers;
+
+import android.content.Intent;
+
+import net.ianrabt.wpa.FBRepository;
+import net.ianrabt.wpa.Repository;
+import net.ianrabt.wpa.models.HabitCellModel;
+import net.ianrabt.wpa.models.HabitModel;
+import net.ianrabt.wpa.views.CreateHabitActivity;
+import net.ianrabt.wpa.views.HabitsActivity;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class HabitsController {
+
+    private FBRepository mRepository;
+    private Calendar sCalendar = Calendar.getInstance();
+    public ArrayList<HabitCellModel> habitsList = new ArrayList<HabitCellModel>();
+    private HabitsActivity activity;
+
+    public HabitsController(HabitsActivity activity){
+        this.activity = activity;
+        mRepository = new FBRepository();
+        mRepository.setDelegate(activity);
+
+    }
+
+    public void queryHabits(){
+        String day = Integer.toString(sCalendar.get(Calendar.DAY_OF_WEEK));
+        mRepository.getHabitsByDay(day);
+    }
+
+    public void handleHabitResponse(ArrayList<HabitModel> habitResponse) {
+        for (int i = 0; i < habitResponse.size(); i++) {
+            habitsList.add(new HabitCellModel(habitResponse.get(i)));
+        }
+    }
+
+    public String getDay(){
+        return sCalendar.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.LONG, Locale.getDefault());
+    }
+
+    public void segueToCreateHabitActivity(){
+        Intent newActivity = new Intent(activity, CreateHabitActivity.class);
+        activity.startActivity(newActivity);
+    }
+
+}
---
+++ app/src/main/java/net/ianrabt/wpa/controllers/LoginController.java
@@ -0,0 +1,106 @@
+package net.ianrabt.wpa.controllers;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+
+import net.ianrabt.wpa.R;
+import net.ianrabt.wpa.views.HabitsActivity;
+import net.ianrabt.wpa.views.LoginActivity;
+
+public class LoginController {
+
+    private static final String TAG = "LoginController";
+    FirebaseAuth mAuth;
+    GoogleSignInClient mGoogleSignInClient;
+    private final static int RC_SIGN_IN = 2;
+    LoginActivity activity;
+
+    public LoginController(LoginActivity activity) {
+        this.activity = activity;
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(activity.getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        // Build a GoogleSignInClient with the options specified by gso.
+        mGoogleSignInClient = GoogleSignIn.getClient(activity, gso);
+
+        this.mAuth = FirebaseAuth.getInstance();
+    }
+
+    public void getCurrentUser(){
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            activity.startActivity(new Intent(activity, HabitsActivity.class));
+        }
+    }
+
+    public void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        activity.startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account);
+            } catch (ApiException e) {
+                // Google Sign In failed, update UI appropriately
+                Log.w(TAG, "Google sign in failed", e);
+                // ...
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(activity, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            //updateUI(user);
+                            if(user != null){
+                                activity.startActivity(new Intent(activity, HabitsActivity.class));
+                            }
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            //Snackbar.make(findViewById(R.id.main_layout), "Authentication Failed.", Snackbar.LENGTH_SHORT).show();
+                            //updateUI(null);
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+
+}
