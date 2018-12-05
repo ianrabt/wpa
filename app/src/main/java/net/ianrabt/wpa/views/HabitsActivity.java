dd0da27f9385ec15b91dc638bd7668cd28c80d5d
Alberto Garcia <garcia.alberto1211@gmail.com>
2018-12-05 03:56:32 -0800

    added menu with functional settings and sign out buttons

--- app/src/main/java/net/ianrabt/wpa/views/HabitsActivity.java
+++ app/src/main/java/net/ianrabt/wpa/views/HabitsActivity.java
@@ -1,17 +1,25 @@
 package net.ianrabt.wpa.views;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import net.ianrabt.wpa.R;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class HabitsActivity extends AppCompatActivity {
+
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mAuth = FirebaseAuth.getInstance();
         setContentView(R.layout.activity_habits);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -26,6 +34,32 @@ public class HabitsActivity extends AppCompatActivity {
         viewPager.setAdapter(adapter);
         viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
         tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(viewPager));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_home, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.action_settings:
+                Intent intent_1 = new Intent(this, SettingsActivity.class);
+                this.startActivity(intent_1);
+                break;
+            case R.id.action_logout:
+                mAuth.signOut();
+                Intent intent_2 = new Intent(this, LoginActivity.class);
+                this.startActivity(intent_2);
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+        return true;
     }
 }
 
